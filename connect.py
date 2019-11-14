96e95bcb1379b5e9c8a23a36c048812dc5f34b77
RuiZhang <ruiruicactus@hotmail.com>
2019-11-14 00:28:57 -0800

    function1

--- connect.py
+++ connect.py
@@ -12,7 +12,9 @@ cur = conn.cursor()
 
 
 def get_table(table_name):
-    cur.execute(SQL("select * from %s;").format(Identifier(table_name),))
+    sql = "select * from %s;"
+    data = (table_name,)
+    cur.execute(sql, data)
 
 country = 'country'
 get_table(country)
