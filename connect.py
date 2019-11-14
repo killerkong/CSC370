ae8c372a9c365f55449cc45397e18110cb24360f
Rui Zhang <ruiruicactus@hotmail.com>
2019-11-14 12:13:47 -0800

    sdf

--- connect.py
+++ connect.py
@@ -20,7 +20,7 @@ def get_table(table_name):
     print_cur(cur)
 
 def insert(table_name, values):
-    cur.execute("insert into %s values%s;", (table_name, values))
+    cur.execute(SQL("insert into {} values%s;").format(Identifier(table_name)),values)
     
 table_name = 'country'
 values = ('default',"'Korea'")
