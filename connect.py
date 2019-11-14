4a9cb4dea71715d84bd031cceb32c2db98b5f693
RuiZhang <ruiruicactus@hotmail.com>
2019-11-14 00:07:49 -0800

    function1

--- connect.py
+++ connect.py
@@ -12,9 +12,9 @@ cur = conn.cursor()
 
 
 def get_table(table_name):
-    statement = cur.mogrify("select * from %s, ;", (table_name,))
-    print(statement)
-    cur.execute(statement)
+    #statement = cur.mogrify("select * from %s;", (table_name,))
+    #print(statement)
+    cur.execute("select * from %s;", (table_name))
 
 #cur.execute("insert into country values(default, 'Korea');")
 country = 'country'
