ac60b1c6bb1e2536f31aa47be11d604ac033da8a
RuiZhang <ruiruicactus@hotmail.com>
2019-11-14 00:09:51 -0800

    function1

--- connect.py
+++ connect.py
@@ -17,8 +17,8 @@ def get_table(table_name):
     cur.execute("select * from %s;", (table_name,))
 
 #cur.execute("insert into country values(default, 'Korea');")
-country = 'country'
-get_table('country')
+country = input('Enter you country: ')
+get_table(country)
 
 conn.commit()
 cur.close()
