58765d1b341ee3d2d4cef3fac1c8a0ac19b03856
RuiZhang <ruiruicactus@hotmail.com>
2019-11-14 00:04:43 -0800

    function1

--- connect.py
+++ connect.py
@@ -18,7 +18,7 @@ def get_table(table_name):
 
 #cur.execute("insert into country values(default, 'Korea');")
 country = 'country'
-get_table(country)
+get_table('country')
 
 conn.commit()
 cur.close()
