7222a12eddb522b1137843796b26891bf3bf1aef
Rui Zhang <ruiruicactus@hotmail.com>
2019-11-14 11:59:59 -0800

    sdf

--- encode.py
+++ encode.py
@@ -8,7 +8,7 @@ print(hash)
 password = "Rz1995227"
 
 def hash(value):
-    code = ((127*value + 31) % 257) % 254
+    code = ((127*value + 31) % 257) % 252 + 1
     return chr(code)
 
 def encode(password):
@@ -17,3 +17,11 @@ def encode(password):
     return ''.join([elem for elem in encode])
     
 print(encode(password))
+
+def insert(table_name, values):
+    sql = "insert into {} values {}".format(table_name, values)
+    print(sql)
+    
+table_name = "country"
+values = ("Canada", 12, 34, "English")
+insert(table_name, values)
